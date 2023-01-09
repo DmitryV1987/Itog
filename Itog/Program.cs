a075c6c1e0644fa9a7452074182bfc081e0d7d6e
DmitryV1987 <cocagreen@mail.ru>
2023-01-09 15:05:11 +0500

    Initial commit

---
+++ Itog/Program.cs
@@ -0,0 +1,15 @@
+﻿string[] input = { "1234", "1567", "-2", "computer science" };
+string[] output = new string[input.Length];
+int count = 0;
+
+for (int i = 0; i < input.Length; i++)
+{
+    if (input[i].Length <= 3)
+    {
+        output[count] = input[i];
+        count++;
+    }
+}
+
+string[] result = new string[count];
+Array.Copy(output, result, count);
